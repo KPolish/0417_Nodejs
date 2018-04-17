6f840eeb7aec65ce5afde7331503cba5056d5dda
KPolish <com94ok@naver.com>
2018-04-17 11:33:54 +0900

    20180417_Nodejs#6

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "20180417_Nodejs#5"
+git commit -m "20180417_Nodejs#6"
 git push -u origin master
--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-console.log('hello, world!');
-
-const circle = require('./circle');  // ./ 점 하나는 현재 디렉토리, 두개는 상위폴더
-console.log('Area of a circle = ' + circle.area(3));
-console.log('Circumference of a circle = ' + circle.circum(3));
-console.log('PI = ' + circle.PI);
-
-const square = require('./square');
-let mysquare = square(5);
-mysquare.width = 10;
-console.log('Area of a square = ' + mysquare.area());
-console.log('Circumference of a square = ' + mysquare.circum());
-
-const os = require('os');
-console.log(os.type());
-console.log(os.cpus());
+const http = require('http');
+let server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.write('Hello, world!');
+    res.end();
+});
+server.listen(8000, 'localhost');
+console.log('Server is running at localhost:8000');
