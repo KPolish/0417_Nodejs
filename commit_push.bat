0937c9cf34b3355336eee9f6269ecc6003d15517
KPolish <com94ok@naver.com>
2018-04-17 10:41:58 +0900

    20180417_Nodejs#2

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "20180417_Nodejs#1"
+git commit -m "20180417_Nodejs#2"
 git push -u origin master
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log('hello, world!');
-console.log('Node.js version : ' + process.version);
-console.log(process.memoryUsage());
+const circle = require('./circle');  // ./ 점 하나는 현재 디렉토리, 두개는 상위폴더
+console.log('Area of a circle = ' + circle.area(3));
+console.log('Circumference of a circle = ' + circle.circum(3));
+console.log('PI = ' + circle.PI);
